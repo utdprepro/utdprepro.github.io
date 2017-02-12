abbf4b793dfdf5f928488f95e2e86fff0a72a6cb
thepaperpilot <thepaperpilot@gmail.com>
2017-02-11 18:23:32 -0600

    Added an interactive genre shader program

---
+++ _cite/S17/genres.md
@@ -0,0 +1,12 @@
+---
+layout: cite
+page: Pagination
+project: S17
+title: Genre Filters
+author: Anthony Lawn
+media: Interactive
+---
+<div class="interactive">
+<iframe src="/projects/S17/genres_iframe.html"></iframe>
+<a href="#" onclick="toggleFullScreen()" class="fullscreen_btn"><i class="material-icons">settings_overscan</i></a>
+</div>
--- js/init.js
+++ js/init.js
@@ -1,17 +1,55 @@
 $(document).ready(function(){
-    $(".button-collapse").sideNav();
-    $("img:not(.carousel-image)").addClass("materialboxed");
-    $(".materialboxed").materialbox();
-    $('.carousel.carousel-slider').carousel({full_width: true});
+	$(".button-collapse").sideNav();
+	$("img:not(.carousel-image)").addClass("materialboxed");
+	$(".materialboxed").materialbox();
+	$('.carousel.carousel-slider').carousel({full_width: true});
 	$(".dropdown-button").dropdown();
 	$('ul.tabs').tabs();
 	setTimeout(function() {
 		$('.toc-wrapper').pushpin({
 			top: $('header').height() + $('.head').outerHeight(),
-	        bottom: $('footer').first().offset().top
+			bottom: $('footer').first().offset().top
 		});
 	}, 100);
 });
+
+function toggleFullScreen() {
+	if ((document.fullScreenElement && document.fullScreenElement !== null) ||
+		(!document.mozFullScreen && !document.webkitIsFullScreen)) {
+		if (document.documentElement.requestFullScreen) {
+			document.documentElement.requestFullScreen();
+		} else if (document.documentElement.mozRequestFullScreen) {
+			document.documentElement.mozRequestFullScreen();
+		} else if (document.documentElement.webkitRequestFullScreen) {
+			document.documentElement.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);
+		}
+	} else {
+		if (document.cancelFullScreen) {
+			document.cancelFullScreen();
+		} else if (document.mozCancelFullScreen) {
+			document.mozCancelFullScreen();
+		} else if (document.webkitCancelFullScreen) {
+			document.webkitCancelFullScreen();
+		}
+	}
+}
+
+var updateFullScreen = function() {
+	if (document.webkitIsFullScreen || document.mozFullScreen || document.msFullscreenElement) {
+		// Currently fullscreen
+        $('.interactive').addClass('fullscreen');
+    } else {
+    	// Currently not fullscreen
+    	$('.interactive').removeClass('fullscreen');
+    }
+};
+
+if (document.addEventListener) {
+	document.addEventListener('webkitfullscreenchange', updateFullScreen, false);
+    document.addEventListener('mozfullscreenchange', updateFullScreen, false);
+    document.addEventListener('fullscreenchange', updateFullScreen, false);
+    document.addEventListener('MSFullscreenChange', updateFullScreen, false);
+}
 
 var resize = function() {
 	var targetWidth = ($(window).width() > 992 ? 3 : $(window).width() > 600 ? 2 : 1) * $('.images').children()[0].scrollWidth + 1;
@@ -35,6 +73,7 @@ var resize = function() {
 	}
 	$('.images').height(Math.max($('.images').height(), height));
 };
+
 var width = $(window).width();
 window.onresize = function() {
 	if (width != $(window).width())
